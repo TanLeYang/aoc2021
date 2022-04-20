e7c3ac429ab5f581f1527e8e3bdb29e3100611a4
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2022-04-20 21:12:46 +0800

    Complete day 7 part 2

--- day07/main.ml
+++ day07/main.ml
@@ -1,15 +1,22 @@
-let calculate_cost (initial_positions: int list) (destination: int): int =
+let calculate_const_cost (initial_positions: int list) (destination: int): int =
   List.map (fun x -> abs(destination - x))  initial_positions
   |> List.fold_left (+) 0
 
-let cheapest_outcome (positions: int list): int =
-  let max_position = List.fold_left Stdlib.max 0 positions in
+let calculate_linear_cost (initial_positions: int list) (destination: int): int =
+  List.map (fun x ->
+    let n = abs(destination - x) in
+    n * (n + 1) / 2
+  ) initial_positions
+  |> List.fold_left (+) 0
+
+let cheapest_outcome (cost_fn: int list -> int -> int) (positions: int list): int =
+  let max_position = List.fold_left Stdlib.max min_int positions in
   let costs =
     List.init (max_position + 1) (fun x -> x)
-    |> List.map (fun destination -> calculate_cost positions destination)
+    |> List.map (fun destination -> cost_fn positions destination)
   in
 
-  List.fold_left Stdlib.min ((max_position + 1) * (List.length positions)) costs
+  List.fold_left Stdlib.min (max_int) costs
 
 let parse_input (input_file: string): int list =
   let ic = open_in input_file in
@@ -18,6 +25,14 @@ let parse_input (input_file: string): int list =
   |> List.map int_of_string
 
 let () =
-  parse_input "input.txt"
-  |> cheapest_outcome
-  |> print_int
+  let initial_positions = parse_input "input.txt" in
+
+  (* part 1*)
+  cheapest_outcome calculate_const_cost initial_positions
+  |> print_int;
+
+  print_newline();
+
+  (* part 2*)
+  cheapest_outcome calculate_linear_cost initial_positions
+  |> print_int;
